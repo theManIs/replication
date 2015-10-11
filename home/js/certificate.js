9f1913b074d6cb0e3201b4fce0f883f4b2546a04
theManIs <lostlose@yandex.ru>
2015-10-11 21:06:52 +0400

    Добавил сретификаты на домашнюю страницу.

---
+++ home/js/certificate.js
@@ -0,0 +1,2 @@
+$('img[alt=certificate]').click(function(){$('body').append($('<img>').attr('class', 'cert').css({"width" : "1000px", "position" : "fixed", "left": "20%", "top":"20%", "z-index": "10"}).attr("src", $(this).attr("src")));$('#main').add('#bg').css({"opacity":".5"});});
+$('body').on('click', '.cert', function(){$('.cert').click(function(){$(this).remove(); $('#main').add('#bg').css({"opacity":"1"});});});
--- home/js/js/main-script.js
+++ home/js/js/main-script.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
 	$.get('js/js/iconMove.js');
 	$.get('js/js/booksScript.js');
 	$.get('js/js/blogScript.js');
+	
 });
 
+
+	$('img[alt=certificate]').click(function(){$('body').append($('<img>').attr('class', 'cert').css({"width" : "1000px", "position" : "fixed", "left": "20%", "top":"20%", "z-index": "10"}).attr("src", $(this).attr("src")));$('#main').add('#bg').css({"opacity":".5"});});
+$('body').on('click', '.cert', function(){$('.cert').click(function(){$(this).remove(); $('#main').add('#bg').css({"opacity":"1"});});});
