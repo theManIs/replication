8c218dec413ef83cbf1784ec28855e35b4d6994f
theManIs <lostlose@yndex.ru>
2015-11-06 19:30:32 +0300

    1

--- lead/sModule.js
+++ lead/sModule.js
@@ -1,17 +1,26 @@
 
 $(document).ready(function() {
 	var sound = $('a[href$=".mp3"].playlink');
+	$('a[href$=".mp3"].playlink').text('adfadfa');
 	for (i = 0, c = sound.length; i < c; i++) {
-		var newS = $('<li>').append($('<a>').attr('href', sound[i].href).text($(sound[i]).text()));
-		$(newS.find('a')).text($(sound[i]).attr('recordname'));
+		var recordname = $(sound[i]).attr('recordname');
+		//var newS = $('<li>').append($('<a>').attr('href', sound[i].href).text($(sound[i]).text()));
+		//$(newS.find('a')).text($(sound[i]).attr('recordname'));
+		var newS = $('<li>').append($('<a>').attr('href', sound[i].href).text(recordname));
 		$($('.sm2-playlist-bd')[1]).append(newS);
-		
-	}	
+		$($($('.sm2-playlist-bd')[1]).find('a')[i]).click(function() {
+			$('tr.success').removeClass('success');
+			//$('*[recordname="' + recordname + '"').parent().parent().addClass('success');
+			$('*[recordname="' + $($('a[href$=".mp3"].playlink')[i]).attr('recordname') + '"')
+				.parent().parent().addClass('success')
+		});
+	}
+	
 	$('.sm2-bar-ui').css('display', 'none');
 	var clickMp3 = function(o) {
 	$(sound[o]).click(function(event) {
 		event.preventDefault();
-		$('.success').removeClass('success');
+		$('tr.success').removeClass('success');
 		$($(this).parent().parent()).addClass('success');
 		$('.sm2-bar-ui').css('display', 'inline-block');
 		sm2BarPlayers[0].playlistController.playItemByOffset(o + 1);
