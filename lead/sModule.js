db6857c489a452f21c39d6365b1e9c9b06b50658
theManIs <lostlose@yndex.ru>
2015-11-07 14:17:25 +0300

    ADFASDF

--- lead/sModule.js
+++ lead/sModule.js
@@ -1,38 +1,67 @@
-
-$(document).ready(function() {
-	var sound = $('a[href$=".mp3"].playlink');
-	$('a[href$=".mp3"].playlink').text('adfadfa');
-	for (i = 0, c = sound.length; i < c; i++) {
+const DEBUG = true;
+supportFunction = {
+	lightRow: function(i, sound) {
+		var recordname = $(sound[i]).attr('recordname');			
+		$('tr.success').removeClass('success');				
+		$('*[recordname="' + recordname + '"').parent().parent().addClass('success');
+		if (DEBUG === true)
+			console.log(i + ' ' + sound + ' ' + recordname);
+	},
+	clickPlaylist: function(i, sound) {		
+		$($($('.sm2-playlist-bd')[1]).find('a')[i]).click(function() {
+			supportFunction.lightRow(i, sound);
+		});
+	},
+	clickMp3: function(i, sound) {
+		$(sound[i]).click(function(event) {
+			event.preventDefault();
+			supportFunction.lightRow(i, sound);
+			$('.sm2-bar-ui').css('display', 'inline-block');
+			sm2BarPlayers[0].playlistController.playItemByOffset(i + 1);
+		});
+	},
+	buildPlaylist: function(i, sound) {		
 		var recordname = $(sound[i]).attr('recordname');
-		//var newS = $('<li>').append($('<a>').attr('href', sound[i].href).text($(sound[i]).text()));
-		//$(newS.find('a')).text($(sound[i]).attr('recordname'));
 		var newS = $('<li>').append($('<a>').attr('href', sound[i].href).text(recordname));
 		$($('.sm2-playlist-bd')[1]).append(newS);
-		$($($('.sm2-playlist-bd')[1]).find('a')[i]).click(function() {
-			$('tr.success').removeClass('success');
-			//$('*[recordname="' + recordname + '"').parent().parent().addClass('success');
-			$('*[recordname="' + $($('a[href$=".mp3"].playlink')[i]).attr('recordname') + '"')
-				.parent().parent().addClass('success')
-		});
-	}
-	
-	$('.sm2-bar-ui').css('display', 'none');
-	var clickMp3 = function(o) {
-	$(sound[o]).click(function(event) {
-		event.preventDefault();
-		$('tr.success').removeClass('success');
-		$($(this).parent().parent()).addClass('success');
-		$('.sm2-bar-ui').css('display', 'inline-block');
-		sm2BarPlayers[0].playlistController.playItemByOffset(o + 1);
-	});
-	}		
-	for (i = 0, c = sound.length; i < c; i++) {
-		clickMp3(i);
-	}
-	$('a[href="#close"]').click(function(){
+	},
+	clickClose: function() {
+		$('a[href="#close"]').click(function(){
 		sm2BarPlayers[0].actions.stop();
 		$('.success').removeClass('success');
-		$('.sm2-bar-ui').css('display', 'none');
-		
-	});
+		$('.sm2-bar-ui').css('display', 'none');		
+		});
+	},
+	nextStep: function(sound) {
+		$('a[href="#next"]').add('a[href="#prev"]').click(function(){
+			var side = event.target.href.substr(-4);
+			var the = $('tr.success').find('a[href$=".mp3"].playlink').attr('recordname');
+			var playlist = $($('.sm2-playlist-bd')[1]).find('a');
+			if (sound && the && playlist) {
+				for (i = 0, c = playlist.size(); i < c; i++) {
+					if ($(playlist[i]).text() === the)
+						next = i;	
+				}
+				$('tr.success').removeClass('success');
+			}
+			next = (side === 'next') ? next + 1 : next - 1;
+			var recordname = $(sound[next]).attr('recordname');
+			$('*[recordname="' + recordname + '"').parent().parent().addClass('success');
+			if (DEBUG === true)
+				console.log('the: ' + the + ' side: ' + side +
+					' sound: ' + sound + ' recordname: ' + recordname + ' next: ' + next);
+			return true;
+		});
+	},
+}
+$(document).ready(function() {
+	var sound = $('a[href$=".mp3"].playlink');
+	for (i = 0, c = sound.length; i < c; i++) {
+		supportFunction.buildPlaylist(i, sound);
+		supportFunction.clickPlaylist(i, sound);
+		supportFunction.clickMp3(i, sound);
+	}
+	supportFunction.nextStep(sound);
+	supportFunction.clickClose();
+	$('.sm2-bar-ui').css('display', 'none');
 });
